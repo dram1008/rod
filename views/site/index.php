e0feb86f4ae675366302cf611714a02b28fa5bec
dram1008 <dram1008@yandex.ru>
2015-10-24 06:10:22 +0300

    sd

--- views/site/index.php
+++ views/site/index.php
@@ -19,11 +19,20 @@ $this->registerJs("$('.carousel').carousel()");
             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
             <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="4"></li>
         </ol>
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
+                <img src="/images/controller/site/index/0.jpg" alt="...">
+
+                <div class="carousel-caption">
+
+                </div>
+            </div>
+            <div class="item">
                 <img src="/images/controller/site/index/1.jpg" alt="...">
 
                 <div class="carousel-caption">
@@ -38,6 +47,13 @@ $this->registerJs("$('.carousel').carousel()");
             </div>
             <div class="item">
                 <img src="/images/controller/site/index/3.jpg" alt="...">
+
+                <div class="carousel-caption">
+
+                </div>
+            </div>
+            <div class="item">
+                <img src="/images/controller/site/index/4.jpg" alt="...">
 
                 <div class="carousel-caption">
 
