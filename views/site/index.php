06ef71bc4dd88c8d5ec28af12a96d9e6bfad41e2
dram1008 <dram1008@yandex.ru>
2015-10-24 06:22:17 +0300

    sd

--- views/site/index.php
+++ views/site/index.php
@@ -14,14 +14,14 @@ $this->registerJs("$('.carousel').carousel()");
     <div id="carousel-example-generic" class="carousel slide thumbnail" data-ride="carousel" style="
     margin-bottom: 60px;
 ">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-        </ol>
+<!--        <!-- Indicators -->-->
+<!--        <ol class="carousel-indicators">-->
+<!--            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>-->
+<!--            <li data-target="#carousel-example-generic" data-slide-to="1"></li>-->
+<!--            <li data-target="#carousel-example-generic" data-slide-to="2"></li>-->
+<!--            <li data-target="#carousel-example-generic" data-slide-to="3"></li>-->
+<!--            <li data-target="#carousel-example-generic" data-slide-to="4"></li>-->
+<!--        </ol>-->
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
