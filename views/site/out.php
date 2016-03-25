4ebd6de2a222ef7cabe22e0a8587bc640a5a9db9
Бог-Творец <god@galaxysss.ru>
2016-03-25 03:03:26 +0300

    init

--- views/site/out.php
+++ views/site/out.php
@@ -81,7 +81,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             </p>
             <?php $this->registerJs("$('.buttonOrder').tooltip()")?>
-            <a href="#" class="btn btn-success btn-lg text-center buttonOrder" title="Заказать" style="width: 100%;">
+            <a href="/contact" class="btn btn-success btn-lg text-center buttonOrder" title="Заказать" style="width: 100%;">
                 100 000 руб.*
             </a>
             <p>* возможен бартер</p>
