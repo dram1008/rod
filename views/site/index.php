8287d81824c5025f7ee4b77d3a765bb4bf883816
Святослав Архангельский <god@galaxysss.ru>
2016-03-28 00:55:48 +0300

    init

--- views/site/index.php
+++ views/site/index.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'ПриРоде';
+$this->title = 'Главная';
 $this->registerJs("$('.carousel').carousel()");
 ?>
 <div class="site-index">
-    <h1 class="page-header"><?= \yii\bootstrap\Html::encode($this->title) ?>
-        <small>Агентство Сохранения Рода</small>
+    <h1 class="page-header">
+        Агентство Сохранения Рода
     </h1>
 
     <?php $this->registerJs("$('.carousel').carousel()"); ?>
